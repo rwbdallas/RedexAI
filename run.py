75a7094a7d19189b75308bd16057c5076d0e5d2b
rwbdallas <rwbdallas@outlook.com>
2025-11-22 11:48:52 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
-import os
 import uvicorn
+import os
+import sys
+
+# Add backend folder to sys.path so Python can find main.py
+sys.path.append(os.path.join(os.path.dirname(__file__), "backend"))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=8000, reload=True)
